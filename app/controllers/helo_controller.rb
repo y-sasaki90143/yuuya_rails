323341fa2558437e0b7aac1cc5575149062aadbe
ysasaki <yuuysasaki@pasonatech.co.jp>
2017-08-07 06:05:49 +0000

    Add helo_html.

--- app/controllers/helo_controller.rb
+++ app/controllers/helo_controller.rb
@@ -1,7 +1,15 @@
 class HeloController < ApplicationController
 
     def index
-        render plain:"Hello, This is Rails sample page!"
+        msg = '
+            <html>
+            <body>
+                <h1>Sample Page</h1>
+                <p>this is Rails sample page!</p>
+            </body>
+            </html>
+            '
+        render html: msg.html_safe
     end
     
 end
