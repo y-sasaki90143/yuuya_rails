8624ce76617715c2b13589cb2470a7b43a8bf4db
ysasaki <yuuysasaki@pasonatech.co.jp>
2017-08-07 06:43:53 +0000

    Add index_view.

--- app/controllers/helo_controller.rb
+++ app/controllers/helo_controller.rb
@@ -1,15 +1,6 @@
 class HeloController < ApplicationController
 
     def index
-        msg = '
-            <html>
-            <body>
-                <h1>Sample Page</h1>
-                <p>this is Rails sample page!</p>
-            </body>
-            </html>
-            '
-        render html: msg.html_safe
     end
     
 end
