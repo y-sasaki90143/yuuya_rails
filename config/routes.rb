64469cfccdf75052a9d2a57a6bea838fd316fd2a
ysasaki <yuuysasaki@pasonatech.co.jp>
2017-08-08 05:42:07 +0000

    Add dengon_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'dengonban', to: 'dengonban#index'
+  post 'dengonban', to: 'dengonban#index'
+  get 'dengonban/index'
+  post 'dengonban/index'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     get '/helo/index'
     get 'helo', to: 'helo#index'
