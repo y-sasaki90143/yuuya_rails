03abc7316aed39fd16d0b6067e8f543b3f32e32e
ysasaki <yuuysasaki@pasonatech.co.jp>
2017-08-08 01:18:39 +0000

    Add select_list

--- app/controllers/helo_controller.rb
+++ app/controllers/helo_controller.rb
@@ -3,14 +3,14 @@ class HeloController < ApplicationController
     def index
         if request.post? then
             @title = 'Result'
-            if params['r1'] then
-                @msg = 'you selected: ' + params['r1'] 
+            if params['s1'] then
+                @msg = 'you selected: ' + params['s1'] 
             else
                 @msg = 'not selected...'
             end
         else
             @title = 'Index'
-            @msg = 'Select radio button...'
+            @msg = 'Select List...'
         end
     end
 
